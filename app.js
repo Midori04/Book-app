68b5b6ece91c0582e71ee49637b50662d8f540eb
Christoffer <95276246+Midori04@users.noreply.github.com>
2022-10-19 12:33:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -45,13 +45,13 @@ hideBox.addEventListener('change', function(e){
 });
 
 // filter books
-const searchBar = forms['search-books'].querySelector('input');
-searchBar.addEventListener('keyup', (e) => {
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function (e){
   const term = e.target.value.toLowerCase();
   const books = list.getElementsByTagName('li');
-  Array.from(books).forEach((book) => {
+  Array.from(books).forEach(function(book){
     const title = book.firstElementChild.textContent;
-    if(title.toLowerCase().indexOf(e.target.value) != -1){
+    if(title.toLowerCase().indexOf(term) != -1){
       book.style.display = 'block';
     } else {
       book.style.display = 'none';
